f7836ffe510f5e4fd2962754e7e959e02cebd327
Ali Sher <sans42699@gmail.com>
2026-08-02 00:32:42 -0700

    Minor Tweaks

--- CustomGesturesStatic.py
+++ CustomGesturesStatic.py
@@ -4,7 +4,7 @@ import Accessories as ac
 
 scale = 100
 angle_max = 96
-angle_min = 79
+angle_min = 81
 
 def distance3D(p1, p2):
     return math.hypot(p1.x - p2.x ,p1.y - p2.y, p1.z - p2.z)
@@ -42,6 +42,8 @@ def getExtendedFingers(detected_hand_points, hand_iden):
         gb.rightRaised = raised
 
 def indexFingerPointedANY(detected_hand_points,hand_iden,built_in_closest):
+    if not ((hand_iden == "left" and gb.leftRaised[0] == False) or ((hand_iden == "right" and gb.rightRaised[0] == False))):
+        return "None"
     #Index & middle
     ind_mid = normalized_distance3D(detected_hand_points[8],detected_hand_points[12],detected_hand_points)
 
@@ -54,16 +56,16 @@ def indexFingerPointedANY(detected_hand_points,hand_iden,built_in_closest):
     #thumb & middle
     thmb_mid = normalized_distance3D(detected_hand_points[2],detected_hand_points[12],detected_hand_points)
 
-    thmb2_12 = normalized_distance3D(detected_hand_points[2],detected_hand_points[12],detected_hand_points)
-    thmb3_12 = normalized_distance3D(detected_hand_points[3],detected_hand_points[12],detected_hand_points)
-    thmb4_12 = normalized_distance3D(detected_hand_points[4],detected_hand_points[12],detected_hand_points)
+#    thmb2_12 = normalized_distance3D(detected_hand_points[2],detected_hand_points[12],detected_hand_points)
+#    thmb3_12 = normalized_distance3D(detected_hand_points[3],detected_hand_points[12],detected_hand_points)
+#    thmb4_12 = normalized_distance3D(detected_hand_points[4],detected_hand_points[12],detected_hand_points)
 #    print("thmb2_12 : ",thmb2_12)
 #    print("thmb3_12 : ",thmb3_12)
 #    print("thmb4_12 : ",thmb4_12)
 
-    if 150>=ind_mid>=90 and 160>=ind_wrd>=100 and 175>=ind_pnky>=100 and 60>=thmb_mid>=0 and 18<=thmb2_12<=39 and 18<=thmb3_12<=50 and 18<=thmb4_12<=52:
-        angle = ac.getAngle(detected_hand_points[8],detected_hand_points[5],detected_hand_points[13],gb.isAngleBaseHorizontal)
-#        print(hand_iden, " angle : ",angle)        
+    angle = ac.getAngle(detected_hand_points[8],detected_hand_points[5],detected_hand_points[9],gb.isAngleBaseHorizontal)
+    print(hand_iden, " angle : ",angle)        
+    if 150>=ind_mid>=90 and 160>=ind_wrd>=100 and 175>=ind_pnky>=100 and 60>=thmb_mid>=0:# and 18<=thmb2_12<=43 and 18<=thmb3_12<=53 and 18<=thmb4_12<=55:
         if angle_min<angle<angle_max:
             return "Index_Up"
         elif (angle>=angle_max and hand_iden == "right") or (angle<=angle_min and hand_iden == "left"):
@@ -72,7 +74,7 @@ def indexFingerPointedANY(detected_hand_points,hand_iden,built_in_closest):
             return "Right_Lean"
 
     if built_in_closest == "Pointing_Up":
-        return "Index_Up"
+        return "EPIC FAIL"
     return "None"
 
 def fairs_pinch_perfect(detected_hand_points,hand_iden,built_in_closest):
